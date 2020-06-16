3639cb34dc15531c85a88f2491294b06137b2bd8
Cjkkkk <shanbinke@gmail.com>
2020-06-17 00:34:35 +0800

    omit unnecessary parameter when init template

--- main.cpp
+++ main.cpp
@@ -15,33 +15,33 @@ class A {
         int32_t c;
         A() {}
         size_t do_pack_size() const {
-            size_t payload_size = pack_size<uint32_t, INT_32>(generate_key<INT_32>(0));
-            payload_size += pack_size<int32_t, INT_32>(a);
-            payload_size += pack_size<uint32_t, INT_32>(generate_key<INT_32>(1));
-            payload_size += pack_size<int32_t, INT_32>(b);
-            payload_size += pack_size<uint32_t, INT_32>(generate_key<INT_32>(2));
-            payload_size += pack_size<int32_t, INT_32>(c);
+            size_t payload_size = pack_size<INT_32>(generate_key<INT_32>(0));
+            payload_size += pack_size<INT_32>(a);
+            payload_size += pack_size<INT_32>(generate_key<INT_32>(1));
+            payload_size += pack_size<INT_32>(b);
+            payload_size += pack_size<INT_32>(generate_key<INT_32>(2));
+            payload_size += pack_size<INT_32>(c);
             return payload_size;
         }
 
         size_t do_pack(uint8_t* buf) const {
             size_t payload_size = pack_key<INT_32>(buf, 0);
-            payload_size += pack<int32_t, INT_32>(buf + payload_size, a);
+            payload_size += pack<INT_32>(buf + payload_size, a);
             payload_size += pack_key<INT_32>(buf + payload_size, 1);
-            payload_size += pack<int32_t, INT_32>(buf + payload_size, b);
+            payload_size += pack<INT_32>(buf + payload_size, b);
             payload_size += pack_key<INT_32>(buf + payload_size, 2);
-            payload_size += pack<int32_t, INT_32>(buf + payload_size, c);
+            payload_size += pack<INT_32>(buf + payload_size, c);
             return payload_size;
         }
 
         size_t do_unpack(uint8_t* buf, size_t& payload_size) {
             uint32_t field_number = 0;
             size_t offset = unpack_key<INT_32>(buf, field_number);
-            offset += unpack<int32_t, INT_32>(buf + offset, a);
+            offset += unpack<INT_32>(buf + offset, a);
             offset += unpack_key<INT_32>(buf + offset, field_number);
-            offset += unpack<int32_t, INT_32>(buf + offset, b);
+            offset += unpack<INT_32>(buf + offset, b);
             offset += unpack_key<INT_32>(buf + offset, field_number);
-            offset += unpack<int32_t, INT_32>(buf + offset, c);
+            offset += unpack<INT_32>(buf + offset, c);
             return payload_size;
         }
 };
@@ -54,21 +54,21 @@ class B {
         int32_t c;
         B() {}
         size_t do_pack_size() const {
-            size_t payload_size = pack_size<A, MESSAGE>(a) + pack_size<int32_t, INT_32>(b) + pack_size<int32_t, INT_32>(c);
+            size_t payload_size = pack_size<MESSAGE>(a) + pack_size<INT_32>(b) + pack_size<INT_32>(c);
             return payload_size;
         }
 
         size_t do_pack(uint8_t* buf) const {
-            size_t payload_size = pack<A, MESSAGE>(buf, a);
-            payload_size += pack<int32_t, INT_32>(buf + payload_size, b);
-            payload_size += pack<int32_t, INT_32>(buf + payload_size, c);
+            size_t payload_size = pack<MESSAGE>(buf, a);
+            payload_size += pack<INT_32>(buf + payload_size, b);
+            payload_size += pack<INT_32>(buf + payload_size, c);
             return payload_size;
         }
 
         size_t do_unpack(uint8_t* buf, size_t& payload_size) {
-            size_t offset = unpack<A, MESSAGE>(buf, a);
-            offset += unpack<int32_t, INT_32>(buf + offset, b);
-            offset += unpack<int32_t, INT_32>(buf + offset, c);
+            size_t offset = unpack<MESSAGE>(buf, a);
+            offset += unpack<INT_32>(buf + offset, b);
+            offset += unpack<INT_32>(buf + offset, c);
             return payload_size;
         }
 };
@@ -79,19 +79,19 @@ class C {
         int32_t b;
         C() {}
         size_t do_pack_size() const {
-            size_t payload_size = pack_size<Array<int32_t, INT_32>, ARRAY>(a) + pack_size<int32_t, INT_32>(b);
+            size_t payload_size = pack_size<ARRAY>(a) + pack_size<INT_32>(b);
             return payload_size;
         }
 
         size_t do_pack(uint8_t* buf) const {
-            size_t payload_size = pack<Array<int32_t, INT_32>, ARRAY>(buf, a);
-            payload_size += pack<int32_t, INT_32>(buf + payload_size, b);
+            size_t payload_size = pack<ARRAY>(buf, a);
+            payload_size += pack<INT_32>(buf + payload_size, b);
             return payload_size;
         }
 
         size_t do_unpack(uint8_t* buf, size_t& payload_size) {
-            size_t offset = unpack<Array<int32_t, INT_32>, ARRAY>(buf, a);
-            offset += unpack<int32_t, INT_32>(buf + offset, b);
+            size_t offset = unpack<ARRAY>(buf, a);
+            offset += unpack<INT_32>(buf + offset, b);
             return payload_size;
         }
     
@@ -99,38 +99,38 @@ class C {
 
 int main() {
     int32_t data = -300;
-    size_t ps = pack_size<int32_t, INT_32>(data);
+    size_t ps = pack_size<INT_32>(data);
     std::cout << "pack_size: " << ps << "\n";
     uint8_t* buf = new uint8_t[ps];
-    pack<int32_t, INT_32>(buf, data);
-    unpack<int32_t, INT_32>(buf, data);
+    pack<INT_32>(buf, data);
+    unpack<INT_32>(buf, data);
     std::cout << data << "\n";
     delete[] buf;
 
     int32_t data1 = -320;
-    ps = pack_size<int32_t, SINT_32>(data1);
+    ps = pack_size<SINT_32>(data1);
     std::cout << "pack_size: " << ps << "\n";
     uint8_t* buf1 = new uint8_t[ps];
-    pack<int32_t, SINT_32>(buf1, data1);
-    unpack<int32_t, SINT_32>(buf1, data1);
+    pack<SINT_32>(buf1, data1);
+    unpack<SINT_32>(buf1, data1);
     std::cout << data1 << "\n";
     delete[] buf1;
 
     float data2 = 3.2;
-    ps = pack_size<float, FLOAT>(data2);
+    ps = pack_size<FLOAT>(data2);
     std::cout << "pack_size: " << ps << "\n";
     uint8_t* buf2 = new uint8_t[ps];
-    pack<float, FLOAT>(buf2, data2);
-    unpack<float, FLOAT>(buf2, data2);
+    pack<FLOAT>(buf2, data2);
+    unpack<FLOAT>(buf2, data2);
     std::cout << data2 << "\n";
     delete[] buf2;
 
     double data3 = 3.4;
-    ps = pack_size<double, DOUBLE>(data3);
+    ps = pack_size<DOUBLE>(data3);
     std::cout << "pack_size: " << ps << "\n";
     uint8_t* buf3 = new uint8_t[ps];
-    pack<double, DOUBLE>(buf3, data3);
-    unpack<double, DOUBLE>(buf3, data3);
+    pack<DOUBLE>(buf3, data3);
+    unpack<DOUBLE>(buf3, data3);
     std::cout << data3 << "\n";
     delete[] buf3;
 
@@ -139,12 +139,12 @@ int main() {
     data4.a = 10;
     data4.b = 15;
     data4.c = 20;
-    ps = pack_size<A, MESSAGE>(data4);
+    ps = pack_size<MESSAGE>(data4);
     std::cout << "pack_size: " << ps << "\n";
     uint8_t* buf4 = new uint8_t[ps];
-    pack<A, MESSAGE>(buf4, data4);
+    pack<MESSAGE>(buf4, data4);
     // unpack
-    unpack<A, MESSAGE>(buf4, data4);
+    unpack<MESSAGE>(buf4, data4);
     std::cout << data4.a << "\n";
     std::cout << data4.b << "\n";
     std::cout << data4.c << "\n";
@@ -159,12 +159,12 @@ int main() {
     data5.a = a;
     data5.b = 15;
     data5.c = 20;
-    ps = pack_size<B, MESSAGE>(data5);
+    ps = pack_size<MESSAGE>(data5);
     std::cout << "pack_size: " << ps << "\n";
     uint8_t* buf5 = new uint8_t[ps];
-    pack<B, MESSAGE>(buf5, data5);
+    pack<MESSAGE>(buf5, data5);
     // unpack
-    unpack<B, MESSAGE>(buf5, data5);
+    unpack<MESSAGE>(buf5, data5);
     std::cout << data5.a.a << "\n";
     std::cout << data5.a.b << "\n";
     std::cout << data5.a.c << "\n";
@@ -179,13 +179,13 @@ int main() {
     data6.a.data.push_back(15);
     data6.a.data.push_back(20);
     data6.b = 40;
-    ps = pack_size<C, MESSAGE>(data6);
+    ps = pack_size<MESSAGE>(data6);
     std::cout << "pack_size: " << ps << "\n";
     uint8_t* buf6 = new uint8_t[ps];
-    pack<C, MESSAGE>(buf6, data6);
+    pack<MESSAGE>(buf6, data6);
     // unpack
     C data6_1;
-    unpack<C, MESSAGE>(buf6, data6_1);
+    unpack<MESSAGE>(buf6, data6_1);
     std::cout << data6_1.a.data[0] << "\n";
     std::cout << data6_1.a.data[1] << "\n";
     std::cout << data6_1.a.data[2] << "\n";
--- pack.hpp
+++ pack.hpp
@@ -6,7 +6,7 @@
 #include "./types.hpp"
 
 namespace navi_trans {
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_pack(uint8_t* buf, const T& o_data, VAR_INT) {
         uint64_t data;
         if (is_zigzag<U>::value) {
@@ -30,7 +30,7 @@ namespace navi_trans {
         return size;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_unpack(uint8_t* buf, T& o_data, VAR_INT) {
         uint64_t data = 0;
         size_t size = 0;
@@ -50,72 +50,72 @@ namespace navi_trans {
     }
 
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_pack(uint8_t* buf, const T& o_data, BITS_32) {
         memcpy(buf, &o_data, 4);
         return 4;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_unpack(uint8_t* buf, T& o_data, BITS_32) {
         memcpy(&o_data, buf, 4);
         return 4;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_pack(uint8_t* buf, const T& o_data, BITS_64) {
         memcpy(buf, &o_data, 8);
         return 8;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_unpack(uint8_t* buf, T& o_data, BITS_64) {
         memcpy(&o_data, buf, 8);
         return 8;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_pack(uint8_t* buf, const T& o_data, LENGTH_DELIMITED) {
         size_t payload_size = o_data.do_pack(buf + 4);
-        do_pack<size_t, FIXED_32>(buf, payload_size, BITS_32());
+        do_pack<FIXED_32>(buf, payload_size, BITS_32());
         return payload_size + 4;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_unpack(uint8_t* buf, T& o_data, LENGTH_DELIMITED) {
         // o_data = new typename std::remove_pointer<T>::type;
         size_t payload_size;
-        do_unpack<size_t, FIXED_32>(buf, payload_size, BITS_32());
+        do_unpack<FIXED_32>(buf, payload_size, BITS_32());
         o_data.do_unpack(buf + 4, payload_size);
         return payload_size + 4;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t pack (uint8_t* buf, const T& o_data) {
-        return do_pack <T,U> (buf, o_data, typename wire_type_trait<U>::wire_type());
+        return do_pack <U, T> (buf, o_data, typename wire_type_trait<U>::wire_type());
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t unpack (uint8_t* buf, T& o_data) {
-        return do_unpack <T,U> (buf, o_data, typename wire_type_trait<U>::wire_type());
+        return do_unpack <U, T> (buf, o_data, typename wire_type_trait<U>::wire_type());
     }
     // ------------------
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_pack_size(const T& o_data, LENGTH_DELIMITED) {
         return o_data.do_pack_size() + 4;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_pack_size(const T& o_data, BITS_32) {
         return 4;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_pack_size(const T& o_data, BITS_64) {
         return 8;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t do_pack_size(const T& o_data, VAR_INT) {
         uint64_t data;
         if (is_zigzag<U>::value) {
@@ -134,15 +134,15 @@ namespace navi_trans {
         return size;
     }
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     size_t pack_size(const T& o_data) {
-        return do_pack_size <T,U> (o_data, typename wire_type_trait<U>::wire_type());
+        return do_pack_size <U, T> (o_data, typename wire_type_trait<U>::wire_type());
     }
 
-    template <typename T, typename U, size_t...>
+    template <typename U, typename T, size_t...>
     class Array{};
 
-    template <typename T, typename U>
+    template <typename U, typename T>
     class Array<T, U>{
         public:
             std::vector<T> data;
@@ -150,7 +150,7 @@ namespace navi_trans {
             size_t do_pack_size() const {
                 size_t payload_size = 0;
                 for (auto iter = std::begin(data); iter != std::end(data); ++iter) {
-                    payload_size += pack_size<T, U>(*iter);
+                    payload_size += pack_size<U, T>(*iter);
                 }
                 return payload_size;
             }
@@ -158,7 +158,7 @@ namespace navi_trans {
             size_t do_pack(uint8_t* buf) const {
                 size_t payload_size = 0;
                 for (auto iter = std::begin(data); iter != std::end(data); ++iter) {
-                    payload_size += pack<T, U>(buf + payload_size, *iter);
+                    payload_size += pack<U, T>(buf + payload_size, *iter);
                 }
                 return payload_size;
             }
@@ -167,7 +167,7 @@ namespace navi_trans {
                 size_t offset = 0;
                 while(offset < payload_size) {
                     T t = T();
-                    offset += unpack<T, U>(buf + offset, t);
+                    offset += unpack<U, T>(buf + offset, t);
                     data.push_back(std::move(t));
                 }
                 return payload_size;
@@ -181,14 +181,14 @@ namespace navi_trans {
 
     template <typename T>
     size_t pack_key(uint8_t* buf, const uint32_t& field_number) {
-        return pack<uint32_t, INT_32>(buf, generate_key<T>(field_number));
+        return pack<INT_32>(buf, generate_key<T>(field_number));
     }
 
     template <typename T>
     size_t unpack_key(uint8_t* buf, uint32_t& field_number) {
         uint32_t data;
         uint8_t wire_type_value;
-        size_t size = unpack<uint32_t, INT_32>(buf, data);
+        size_t size = unpack<INT_32>(buf, data);
         field_number = data >> 3;
         wire_type_value = data & 0x7;
         if (wire_type_value != wire_type_trait<T>::wire_type::wire_type_value) {
